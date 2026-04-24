f447458702002d9ce51117b52c039eca71fd46ca
Omar E. Khalifa <omar.khalifa.dev@gmail>
2026-04-25 02:31:23 +0300

    feat: add ButtonModel class

---
+++ lib/models/button_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ButtonModel {
+  final Widget child;
+  final Color buttonColor;
+
+  const ButtonModel({
+    required this.child,
+    required this.buttonColor,
+  });
+}
