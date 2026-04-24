0a9787ea41bf253fda1492add012caa1ad469a37
Omar E. Khalifa <omar.khalifa.dev@gmail>
2026-04-25 00:17:03 +0300

    intial structure commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:currency_calculator/views/currency_calculator_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CurrencyCalculator());
+}
+
+class CurrencyCalculator extends StatelessWidget {
+  const CurrencyCalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CurrencyCalculatorView(),
+    );
+  }
+}
