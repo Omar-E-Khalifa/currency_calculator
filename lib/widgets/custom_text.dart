406d93817c41bccac53adea5fa899dd85c455abc
Omar E. Khalifa <omar.khalifa.dev@gmail>
2026-04-25 14:07:24 +0300

    feat: add fontSize and fontWeight params to CustomText

--- lib/widgets/custom_text.dart
+++ lib/widgets/custom_text.dart
@@ -3,23 +3,24 @@ import 'package:flutter/material.dart';
 /// Reusable widget that carries the design of the Text widget, mainly used in the buttons_list
 
 class CustomText extends StatelessWidget {
-  const CustomText({
-    super.key,
-    required this.text,
-    this.textColor = Colors.white,
-  });
+  const CustomText(
+      {super.key,
+      required this.text,
+      this.textColor = Colors.white,
+      this.fontSize = 20,
+      this.fontWeight = FontWeight.normal});
 
   final String text;
   final Color textColor;
+  final double fontSize;
+  final FontWeight fontWeight;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
       style: TextStyle(
-        color: textColor,
-        fontSize: 20,
-      ),
+          color: textColor, fontSize: fontSize, fontWeight: fontWeight),
     );
   }
 }
