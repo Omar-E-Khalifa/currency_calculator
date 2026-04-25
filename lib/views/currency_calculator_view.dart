2a882ea0a3e814742a709e8401293aac4ce7b0fd
Omar E. Khalifa <omar.khalifa.dev@gmail>
2026-04-25 14:07:37 +0300

    feat: add display area to CurrencyCalculatorView

--- lib/views/currency_calculator_view.dart
+++ lib/views/currency_calculator_view.dart
@@ -1,8 +1,8 @@
 import 'package:currency_calculator/data/buttons_list.dart';
 import 'package:currency_calculator/widgets/calculator_button.dart';
 import 'package:currency_calculator/widgets/custom_appbar.dart';
+import 'package:currency_calculator/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
-
 
 /// The main screen for the currency calculator view, it connects the widgets together to form the full view
 
@@ -13,17 +13,47 @@ class CurrencyCalculatorView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(),
-      body: GridView.builder(
-        physics: const NeverScrollableScrollPhysics(), // The calculator buttons should never scroll
-        itemCount: buttonsList.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4,
-          crossAxisSpacing: 25,
-          mainAxisSpacing: 25,
-        ),
-        itemBuilder: (context, index) {
-          return CalculatorButton(button: buttonsList[index]);
-        },
+      body: Column(
+        children: [
+          SizedBox(
+            height: 120, //TODO: make responsive
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  CustomText(
+                    text: '4,500 + 1,200',
+                    fontSize: 24,
+                    textColor: Theme.of(context).colorScheme.surface,
+                  ),
+                  CustomText(
+                    text: '5,700',
+                    fontSize: 28,
+                    textColor: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          GridView.builder(
+            physics:
+                const NeverScrollableScrollPhysics(), // The calculator buttons should never scroll
+            shrinkWrap: true,
+            itemCount: buttonsList.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+              crossAxisSpacing: 25,
+              mainAxisSpacing: 25,
+            ),
+            itemBuilder: (context, index) {
+              return CalculatorButton(button: buttonsList[index]);
+            },
+          ),
+        ],
       ),
     );
   }
