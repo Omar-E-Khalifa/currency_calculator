94cb44e3a26ea8e5bd97e6c03bd44bb2f12a4123
Omar E. Khalifa <omar.khalifa.dev@gmail>
2026-04-28 01:32:24 +0300

    feat: add calculator button logic to DisplayAreaCubit

--- lib/cubits/display_area_cubit/display_area_cubit.dart
+++ lib/cubits/display_area_cubit/display_area_cubit.dart
@@ -1,8 +1,31 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
 
 part 'display_area_state.dart';
 
 class DisplayAreaCubit extends Cubit<DisplayAreaState> {
   DisplayAreaCubit() : super(DisplayAreaState());
+
+  void buttonPressed(String button) {
+    switch (button) {
+      case '=':
+        emit(DisplayAreaState(
+            result: state.result, typedValue: state.typedValue));
+        break;
+      case 'c':
+        emit(DisplayAreaState(typedValue: ''));
+        break;
+      case 'back':
+        emit(DisplayAreaState(
+          result: state.result,
+          typedValue: state.typedValue.isNotEmpty
+              ? state.typedValue.substring(0, state.typedValue.length - 1)
+              : '',
+        ));
+        break;
+
+      default:
+        emit(DisplayAreaState(typedValue: state.typedValue + button));
+        break;
+    }
+  }
 }
