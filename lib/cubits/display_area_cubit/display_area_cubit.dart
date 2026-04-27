ae7508cb89e7cb60b42755d72632be000f2432cc
Omar E. Khalifa <omar.khalifa.dev@gmail>
2026-04-28 01:00:16 +0300

    feat: add DisplayAreaState and DisplayAreaCubit

---
+++ lib/cubits/display_area_cubit/display_area_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'display_area_state.dart';
+
+class DisplayAreaCubit extends Cubit<DisplayAreaState> {
+  DisplayAreaCubit() : super(DisplayAreaState());
+}
---
+++ lib/cubits/display_area_cubit/display_area_state.dart
@@ -0,0 +1,11 @@
+part of 'display_area_cubit.dart';
+
+class DisplayAreaState {
+  final String typedValue;
+  final String result;
+
+  const DisplayAreaState({
+    this.typedValue = '',
+    this.result = '',
+  });
+}
