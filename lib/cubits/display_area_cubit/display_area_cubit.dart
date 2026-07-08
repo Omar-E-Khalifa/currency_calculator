63a70431ca5cf105bf5048628f5402073a9ffc6e
Omar E. Khalifa <omar.khalifa.dev@gmail>
2026-07-08 22:00:03 +0300

    feat: handle post-result input for back button

--- lib/cubits/display_area_cubit/display_area_cubit.dart
+++ lib/cubits/display_area_cubit/display_area_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
 import 'package:math_expressions/math_expressions.dart';
 
@@ -6,28 +8,38 @@ part 'display_area_state.dart';
 class DisplayAreaCubit extends Cubit<DisplayAreaState> {
   DisplayAreaCubit() : super(DisplayAreaState());
 
+  bool isCalculated = false;
+
   void buttonPressed(String button) {
     switch (button) {
       case '=':
         num? calcResult = calculate(state.typedValue);
-
         emit(DisplayAreaState(
             result: calcResult == null ? '' : calcResult.toString(),
             typedValue: state.typedValue));
+        state.result != '' ? isCalculated = true : isCalculated = false;
+
         break;
+
       case 'c':
         emit(DisplayAreaState(typedValue: ''));
+        isCalculated = false;
         break;
+
       case 'back':
-        emit(DisplayAreaState(
-          result: state.result,
-          typedValue: state.typedValue.isNotEmpty
-              ? state.typedValue.substring(0, state.typedValue.length - 1)
-              : '',
-        ));
+        isCalculated
+            ? emit(DisplayAreaState(result: '', typedValue: state.typedValue))
+            : emit(DisplayAreaState(
+                result: '',
+                typedValue: state.typedValue.isNotEmpty
+                    ? state.typedValue.substring(0, state.typedValue.length - 1)
+                    : '',
+              ));
+        isCalculated = false;
         break;
 
       default:
+        isCalculated = false;
         emit(DisplayAreaState(typedValue: state.typedValue + button));
         break;
     }
@@ -49,8 +61,9 @@ class DisplayAreaCubit extends Cubit<DisplayAreaState> {
       if (eval.isInfinite || eval.isNaN) {
         return null;
       }
-      num formatted  = eval % 1 == 0 ? eval.toInt() : eval; //return int or double
-      return formatted ;
+      num formatted =
+          eval % 1 == 0 ? eval.toInt() : eval; //return int or double
+      return formatted;
     } on FormatException {
       return null;
     } on Exception catch (e) {
