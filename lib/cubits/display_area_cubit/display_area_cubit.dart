88bbb4f62fb793e934e01c136d7a4e048cee8af1
Omar E. Khalifa <omar.khalifa.dev@gmail>
2026-07-08 05:10:17 +0300

    feat: use math_expressions package

--- lib/cubits/display_area_cubit/display_area_cubit.dart
+++ lib/cubits/display_area_cubit/display_area_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 part 'display_area_state.dart';
 
@@ -8,8 +9,13 @@ class DisplayAreaCubit extends Cubit<DisplayAreaState> {
   void buttonPressed(String button) {
     switch (button) {
       case '=':
+        num? calcResult = calculate(state.typedValue);
         emit(DisplayAreaState(
-            result: state.result, typedValue: state.typedValue));
+            result: calcResult == null
+                ? ''
+                : calcResult
+                    .toString(), //if a formatExcpetion happend, the equal button should be ignored or show an empty result
+            typedValue: state.typedValue));
         break;
       case 'c':
         emit(DisplayAreaState(typedValue: ''));
@@ -26,6 +32,30 @@ class DisplayAreaCubit extends Cubit<DisplayAreaState> {
       default:
         emit(DisplayAreaState(typedValue: state.typedValue + button));
         break;
+    }
+  }
+
+  num? calculate(String expression) {
+    try {
+      String updatedValue =
+          expression.replaceAll('×', '*').replaceAll('÷', '/');
+      ExpressionParser p = GrammarParser();
+      Expression exp = p.parse(updatedValue);
+
+      var context = ContextModel();
+
+      // Evaluate expression:
+      var evaluator = RealEvaluator(context);
+      num eval = evaluator.evaluate(exp);
+
+      if (eval.isInfinite || eval.isNaN) {
+        return null;
+      }
+      return eval;
+    } on FormatException {
+      return null;
+    } on Exception catch (e) {
+      throw Exception(e.toString());
     }
   }
 }
