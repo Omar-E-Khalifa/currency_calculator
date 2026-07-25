0599fa51098ac9a806869d83d2648e7a8aa0a9d6
Omar E. Khalifa <omar.khalifa.dev@gmail>
2026-07-26 01:07:08 +0300

    feat: create exchange rate state

---
+++ lib/cubits/exchange_rate_cubit/exchange_rate_state.dart
@@ -0,0 +1,24 @@
+part of 'exchange_rate_cubit.dart';
+
+class ExchangeRateState {}
+
+class ExchangeRateInitial extends ExchangeRateState {}
+
+class ExchangeRateSuccessState extends ExchangeRateState {
+  final String mainCurrencyCode;
+  final double mainValue;
+  final String secCurrencyCode;
+  final double secValue;
+
+  ExchangeRateSuccessState(
+      {required this.mainCurrencyCode,
+      required this.mainValue,
+      required this.secCurrencyCode,
+      required this.secValue});
+}
+
+class ExchangeRateFailureState extends ExchangeRateState {
+  final String errorType;
+
+  ExchangeRateFailureState({required this.errorType});
+}
